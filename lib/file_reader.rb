fa60687a647176326a8596ed89b542f91e9d012f
Lidya1234 <lidya94gher@gmail.com>
2020-10-19 13:55:47 +0300

    Add the check tags method

--- lib/file_reader.rb
+++ lib/file_reader.rb
@@ -1,14 +1,14 @@
 class ReadFile
-    attr_reader :file_lines :file_size
-    def initialize(filepath)
-        @err_msg =''
-        @filepath =filepath
-        begin
-        @file_lines =File.readlines(@filepath)
-        @file_size =@file_lines.size
-        rescue StandardError=> exception
-         @file_lines =[]
-         @err_msg = "The given file path has  an error #{exeption.to_s} "
-        end
+  attr_reader :file_lines, :file_size
+  def initialize(filepath)
+    @err_msg = ''
+    @filepath = filepath
+    begin
+      @file_lines = File.readlines(@filepath)
+      @file_size = @file_lines.size
+    rescue StandardError => exception
+      @file_lines = []
+      @err_msg = "The given file path has  an error #{exeption.to_s} "
     end
+  end
 end
--- lib/linter_checks.rb
+++ lib/linter_checks.rb
@@ -1,5 +1,5 @@
 require_relative 'file_reader.rb'
-attr_reader :file_check :err
+attr_reader :file_check , :err
 class Linter_check
     def initialize(file_path)
   @file_check =ReadFile.new(file_path)
@@ -8,16 +8,38 @@ class Linter_check
 def check_indentation
 
 end
-def trailing_space
+def check_trailing_space
 @file_check.file_line.each_with_index|string ,index|
-{if string.match(\s*$) && !string.strip.empty?
-@err << "#{@file_check.file_path} :#{index +1} :#{string.size-1}: Trailing whitespace detected" }
+{if string.match(/\s*$/) && !string.strip.empty?
+@err << "#{@file_check.file_path} :#{index +1} :#{string.size-1}: Trailing whitespace detected"
 end
+}
+
 end
 def check_tags
+    tags_error(/\(/ ,/\)/ ,'(' ,')')
+    tags_error(/\{/ ,/\}/ ,'{' ,'}')
+    tags_error(/\[/ ,/\]/ ,'[' ,']')
+
 end
 def check_end
 end
 def check_empty_line
+end
+
+def tags_error(*param)
+    opening_tag =[]
+    closing_tag =[]
+    @file_check.file_line.each_with_index|string ,index| do
+        opening_tag << string.scan(arg[0]) 
+        closing_tag << string.scan(arg[1])
+        check = opening_tag.flatten.size <=> closing_tag.flatten.size
+        if check == 1 || check == -1
+            error= "#{@file_check.file_path} :line #{index +1} :Lint/Syntax: Missing or unexpected token"
+         @err << error
+        end
+    end
+ 
+    
 end
 end
