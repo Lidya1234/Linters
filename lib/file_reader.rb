7a3dfd09f7036e4d057b17fc28063b6aa0e86b85
Lidya1234 <lidya94gher@gmail.com>
2020-10-19 12:27:14 +0300

    Add the trailing white space method

--- lib/file_reader.rb
+++ lib/file_reader.rb
@@ -1,13 +1,14 @@
 class ReadFile
+    attr_reader :file_lines :file_size
     def initialize(filepath)
         @err_msg =''
         @filepath =filepath
         begin
         @file_lines =File.readlines(@filepath)
-        @number_of_file_lines =@file_lines.size
+        @file_size =@file_lines.size
         rescue StandardError=> exception
          @file_lines =[]
-         @err_msg "The given file path has  an error #{exeption.to_s} "
+         @err_msg = "The given file path has  an error #{exeption.to_s} "
         end
     end
 end
--- lib/linter_checks.rb
+++ lib/linter_checks.rb
@@ -1,10 +1,23 @@
+require_relative 'file_reader.rb'
+attr_reader :file_check :err
+class Linter_check
+    def initialize(file_path)
+  @file_check =ReadFile.new(file_path)
+  @err =[]
+    end
 def check_indentation
+
 end
 def trailing_space
+@file_check.file_line.each_with_index|string ,index|
+{if string.match(\s*$) && !string.strip.empty?
+@err << "#{@file_check.file_path} :#{index +1} :#{string.size-1}: Trailing whitespace detected" }
+end
 end
 def check_tags
 end
 def check_end
 end
 def check_empty_line
+end
 end
