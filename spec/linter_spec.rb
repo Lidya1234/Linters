034c4bd062a05f0a0fa61d49770fe372781dc0b4
Lidya1234 <lidya94gher@gmail.com>
2020-10-21 12:57:04 +0300

    Modify texts in linter_spec.rb file

--- spec/linter_spec.rb
+++ spec/linter_spec.rb
@@ -4,26 +4,26 @@ describe 'LinterCheck' do
 
   it '#check_trailing_space' do
     lint_spec.check_trailing_space
-    expect(lint_spec.err[0]).eql? '../Lint_bugs/bugs.java : Trailing whitespace detected'
+    expect(lint_spec.err[0]).eql? '../Lint_bugs/bugs.java :Lint/Syntax: Trailing whitespace detected'
   end
   it '#check_tags' do
     lint_spec.check_tags
-    expect(lint_spec.err[0]).eql? '../Lint_bugs/bugs.java : :Lint/Syntax: Missing or unexpected token Braces'
+    expect(lint_spec.err[0]).eql? '../Lint_bugs/bugs.java :Lint/Syntax: Missing or unexpected token Braces'
   end
   it '#check_empty_line' do
     lint_spec.check_empty_line
-    expect(lint_spec.err[0]) != '../Lint_bugs/bugs.java : :Lint/Syntax: Missing or unexpected token Braces'
+    expect(lint_spec.err[0]) != '../Lint_bugs/bugs.java :Lint/Syntax: Missing or unexpected token Braces'
   end
   it '#check_empty_line' do
     lint_spec.check_empty_line
-    expect(lint_spec.err[0]) != 'Empty line at the begning of a class: 4'
+    expect(lint_spec.err[0]) != '../Lint_bugs/bugs.java :Lint/Syntax: Empty line at the begning of a class'
   end
   it '#check_semicolon' do
     lint_spec.check_semicolon
-    expect(lint_spec.err[0]).eql? '../Lint_bugs/bugs.java :7 :Add semicolon '
+    expect(lint_spec.err[0]).eql? '../Lint_bugs/bugs.java :Lint/Syntax: Missing semicolon '
   end
   it '#check_semicolon' do
     lint_spec.check_semicolon
-    expect(lint_spec.err[0]) != '../Lint_bugs/bugs.java :8 :Add semicolon '
+    expect(lint_spec.err[0]) != '../Lint_bugs/bugs.java :Lint/Syntax: Missing semicolon '
   end
 end
